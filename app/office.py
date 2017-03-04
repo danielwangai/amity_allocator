4264e673db817ba8e28496066b110f70bf744de6
Daniel Wangai <daniel.maina@andela.com>
2017-03-04 12:07:12 +0300

    [Feature] Add office class

---
+++ app/office.py
@@ -0,0 +1,9 @@
+from rooms import Room
+
+class Office(Room):
+    '''
+        Office has slots for at most 6 people(Fellows and or Staff)
+    '''
+    room_capacity = 6
+    def init(self):
+        super(Office, self).__init__(room_id, name)
