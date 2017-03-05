e46a5ba46c6ca3092e75865dc93860ef194ce24b
Daniel Wangai <daniel.maina@andela.com>
2017-03-05 17:04:23 +0300

    [Chore] Add add test to prevent duplicate rooms

--- app/test_amity.py
+++ app/test_amity.py
@@ -13,6 +13,15 @@ class TestAmity(unittest.TestCase):
         self.assertTrue(len(self.amity.rooms["office"]), 3)
 
 
+    def test_create_room_does_not_create_duplicate_rooms(self):
+        # create rooom
+        self.assertEqual(self.amity.create_room(["Hogwarts"], "O"), "Room successfully added")
+        # assert that ONE office has been added
+        self.assertTrue(len(self.amity.rooms["office"]) == 1)
+        # try creating a room with the same name
+        self.assertEqual(self.amity.create_room(["Hogwarts"], "O"), "Cannot create duplocate rooms.")
+        # confirm duplicate not added
+        self.assertTrue(len(self.amity.rooms["office"]) == 1)
 
 if __name__== '__main__':
     unittest.main()
