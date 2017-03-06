9feabaad691bf7a00854273858161c9c211820fc
Daniel Wangai <daniel.maina@andela.com>
2017-03-06 16:08:49 +0300

    [Chore] Refactor tests. Prepopulate offices.

--- app/test_amity.py
+++ app/test_amity.py
@@ -5,13 +5,21 @@ import unittest
 class TestAmity(unittest.TestCase):
     def setUp(self):
         self.amity = Amity()
+        # office and its allocations
+        for i in ["Narnia", "Hogwarts", "Platform"]:
+            self.amity.rooms["office"][i] = []
 
     def test_create_room_adds_rooms_successfully(self):
-        offices = ["Hogwarts", "Narnia", "Midgar"]
-        self.assertEqual(self.amity.create_room(offices, "O"), "Room successfully added")
-        # rooms are added to the rooms office dictionary
-        self.assertTrue(len(self.amity.rooms["office"]), 3)
-
+        # check the current number of rooms
+        old_number_of_offices = len(self.amity.rooms["office"])
+        self.assertEqual(old_number_of_offices, 3)
+        # list of new office to be added
+        new_office = ["Hogwarts"]
+        self.assertEqual(self.amity.create_room(new_office, "O"), "Room successfully added")
+        # confirm that extra room has been added
+        new_number_of_offices = self.amity.rooms["office"]
+        # expected - increase by 1
+        self.assertEqual((new_number_of_offices - new_number_of_offices), 1)
 
     def test_create_room_does_not_create_duplicate_rooms(self):
         # create rooom
@@ -33,7 +41,7 @@ class TestAmity(unittest.TestCase):
 
     def test_add_person_creates_person_successfully(self):
         # get number of people(fellows)
-        fellows_before = len(self.amity.people["fellow"]["no_accomodation"])
+        fellows_before = len(self.amity.people["fellows"]["no_accomodation"])
         # create a person(fellow)
         self.assertEqual(self.amity.add_person("Dave", "F", "N"), "Person successfully added")
         # check number of fellows after
