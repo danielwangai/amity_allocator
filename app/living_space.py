9c18778518b005d03cbb41a763a0f68b94d8100b
Daniel Wangai <daniel.maina@andela.com>
2017-03-06 15:19:45 +0300

    [Chore] Refactor models to generate room_id

--- app/living_space.py
+++ app/living_space.py
@@ -6,4 +6,4 @@ class LivingSpace(Room):
     '''
     room_capacity = 6
     def init(self):
-        super(LivingSpace, self).__init__(room_id, name)
+        super(LivingSpace, self).__init__(name)
--- app/rooms.py
+++ app/rooms.py
@@ -2,6 +2,6 @@ class Room(object):
     # set as none to allow inheriting classes to specify its value
     room_capacity = None
 
-    def __init__(self, room_id, name):
-        self.room_id = person_id
+    def __init__(self, name):
+        self.room_id = id(self)
         self.name = name
